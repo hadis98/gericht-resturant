d4be43361f2ac3989f0eb71674ada52134ab21d3
hadis ghafouri <hadisghafouri98@gmail.com>
2022-04-14 11:18:18 +0430

    add FooterOverlay component with styles

--- src/components/Footer/FooterOverlay.jsx
+++ src/components/Footer/FooterOverlay.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React from "react";
 
-import './FooterOverlay.css';
+import "./FooterOverlay.css";
 
 const FooterOverlay = () => (
-  <div>
-    FooterOverlay
+  <div className="app__footeroverlay">
+    <div className="app__footeroverlay-black" />
+    <div className="app__footeroverlay-img app__bg" />
   </div>
 );
 
